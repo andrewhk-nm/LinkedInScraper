fdd83959cc0da5df6a0372f7605a5e183c9bc11d
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-04-10 15:03:40 -0400

    changed it up so that the script outputs on the window in a copiable way

--- nameScraper.js
+++ nameScraper.js
@@ -1,12 +1,14 @@
 // Get the lists of elements in the name and subline classes
 var connectionNames = document.getElementsByClassName("name actor-name");
 var connectionJobs = document.getElementsByClassName("subline-level-1");
+var resultString = "";
 
 // Loop through them and print them to the console.
 // TODO: Make them easier to get to. (document.write did not do what I wanted (permission denied))
-for (var c = 0; c < x.length; ++c) {
-	console.log(connectionNames[c].innerHTML, connectionJobs[c].innerHTML)
+for (var c = 0; c < connectionNames.length; ++c) {
+	resultString += connectionNames[c].innerHTML + "<br>" + connectionJobs[c].innerHTML + "<br><br>"
 }
+document.write(resultString);
 
 
 /*
