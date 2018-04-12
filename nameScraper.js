6891eec3161a255cbde33c0ba5fc2ec5017c0647
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-04-12 15:06:26 -0400

    works as a bookmarklet with console.write. kinda ugly.

--- nameScraper.js
+++ nameScraper.js
@@ -1,20 +1,22 @@
 // Get the lists of elements in the name and subline classes
 var connectionNames = document.getElementsByClassName("name actor-name");
 var connectionJobs = document.getElementsByClassName("subline-level-1");
+var connectionLocation = document.getElementsByClassName("subline-level-2");
 var resultString = "";
 
 // Loop through them and print them to the console.
 // TODO: Make them easier to get to. (document.write did not do what I wanted (permission denied))
 for (var c = 0; c < connectionNames.length; ++c) {
-	resultString += connectionNames[c].innerHTML + "<br>" + connectionJobs[c].innerHTML + "<br><br>"
+	resultString += connectionNames[c].innerHTML + "<br>" + connectionJobs[c].innerHTML + "<br>" + connectionLocation[c].innerHTML + "<br><br>"
 }
 document.write(resultString);
 
 
 /*
 // Single line versions of the above for the bookmarklet.
-var connectionNames = document.getElementsByClassName("name actor-name"); var connectionJobs = document.getElementsByClassName("subline-level-1");for (var c = 0; c < connectionNames.length; ++c) {console.log(connectionNames[c].innerHTML, connectionJobs[c].innerHTML)}
 
-var connectionNames = document.getElementsByClassName("name actor-name"); var connectionJobs = document.getElementsByClassName("subline-level-1");for (var c = 0; c < connectionNames.length; ++c) {document.write(connectionNames[c].innerHTML, connectionJobs[c].innerHTML)}
+var connectionNames = document.getElementsByClassName("name actor-name"); var connectionJobs = document.getElementsByClassName("subline-level-1"); var connectionLocation = document.getElementsByClassName("subline-level-2"); var resultString = ""; for (var c = 0; c < connectionNames.length; ++c) {	resultString += connectionNames[c].innerHTML + "<br>" + connectionJobs[c].innerHTML + "<br>" + connectionLocation[c].innerHTML + "<br><br>"} document.write(resultString);
+
+
 
 */
